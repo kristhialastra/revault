7dbbf0151df6be96abd6e67ad46bf9d686cbd81c
John Allen <dazedmind1@protonmail.com>
2025-05-17 16:17:41 +0800

    form fixes

--- src/app/component/Header.tsx
+++ src/app/component/Header.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import Image from "next/image";
 import icon from "../img/revault-icon.png";
 import Link from "next/link";
-import { FaBuildingColumns } from "react-icons/fa6";
+import { FaBuildingColumns, FaChevronRight } from "react-icons/fa6";
 import { usePathname } from "next/navigation"; // Import the hook
 
 
@@ -20,10 +22,12 @@ const Header = () => {
           </Link>
       </div>
 
-      {!pathname?.startsWith("/admin/login") && (
+      {pathname && !pathname.startsWith("/admin/login") && (
       <span>
-        <Link href={"../admin/login"}>
+        <Link href={"../admin/login"} className="flex items-center gap-4 text-sm text-white-75 hover:text-teal cursor-pointer group">
           <FaBuildingColumns className="text-2xl hover:text-teal cursor-pointer"/>
+          Sign in as Librarian
+          <FaChevronRight className="text-md hover:text-teal cursor-pointer opacity-0 -translate-x-2 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300"/>
         </Link>
       </span>
       )}

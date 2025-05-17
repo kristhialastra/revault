02ffa0e9bd6db08f8336fbbad3d65967021c7b21
KC Durante <kcdurante4@gmail.com>
2025-05-17 12:03:40 +0800

    init

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -267,7 +267,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\allen\\Desktop\\revault\\src\\generated\\prisma",
+      "value": "C:\\Users\\kc\\revault\\src\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -281,11 +281,11 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\allen\\Desktop\\revault\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\kc\\revault\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
